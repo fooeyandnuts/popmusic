11a21c65aa91d1a9c316e1bf6eb616923f23145b
fooeyandnuts <william.sipes@gmail.com>
2014-06-19 00:56:22 -0700

    app started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Popmusic::Application.config.secret_key_base = '087b17ae764714600f17b733116bab8782323ffd853df0fe099ac61e889e4575c305e6718620aebf569acf0d1044774f7df0218954c3c53c30e961478187d0ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Popmusic::Application.config.session_store :cookie_store, key: '_popmusic_session'
